a135e217237c09b021faae1df6020ff646f579aa
allpavel <allpavel@yandex.ru>
2022-07-06 22:02:36 +0300

    added a logo to the sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import { SiTrustedshops } from "react-icons/si";
+import { MdOutlineCancel } from "react-icons/md";
+import { links } from "../../data/dummy";
+import "../App.css";
 
-const Sidebar = () => {
-    return <div>Sidebar</div>;
+const Sidebar: React.FC = () => {
+    const activeMenu = true;
+
+    return (
+        <div className="h-screen p-4">
+            <section className="flex justify-between items-center text-xl dark:text-white text-slate-900">
+                <Link to="/" className="flex items-center" onClick={() => {}}>
+                    <SiTrustedshops className="mr-2" /> Overtea
+                </Link>
+                <div className="relative cursor-pointer" data-closeMenu="Close Menu">
+                    <MdOutlineCancel />
+                </div>
+            </section>
+            <section></section>
+        </div>
+    );
 };
 
 export default Sidebar;
